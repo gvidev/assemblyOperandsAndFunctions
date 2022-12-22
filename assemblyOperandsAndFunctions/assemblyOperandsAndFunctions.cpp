c48ecf4f8d42d249fafeca21567616a3d867e1d3
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2022-12-22 17:43:02 +0200

    adding one more method

--- assemblyOperandsAndFunctions/assemblyOperandsAndFunctions.cpp
+++ assemblyOperandsAndFunctions/assemblyOperandsAndFunctions.cpp
@@ -54,6 +54,8 @@ int perimeter_of_equilateral_triangle(int a) {
 		mul ebx
 	}
 }
+
+//not the best optimized variant because i use integer and possible loss of data
 // calculates perimeter of rectangular triangle
 int perimeter_of_rectangular_triangle(int a, int b) {
 
@@ -76,6 +78,7 @@ int perimeter_of_rectangular_triangle(int a, int b) {
 	c = sqrt(temp);
 	return result += c;
 }
+
 //calculates perimeter of triangle from side length and height
 int perimeter_of_triangle(int a, int h) {
 	__asm {
@@ -90,14 +93,45 @@ int area_of_cube(int a) {
 		mov ebx, 6
 		mul ebx
 		mul a
-
 	}
 }
 
-int perimeter_of_triangle_heron_formula(int a, int b, int c) {
-	__asm {
+ //not the best optimized variant because i use integer and possible loss of data
+ // P = sqrt(s*(s-a)*(s-b)*(s-c) ); s = (a + b + c)/2
+int area_of_triangle_heron_formula(int a, int b, int c) {
 
+	int result = 0;
+	int temp = 0;
+	int const divider = 2;
+
+	__asm {
+		mov eax, a
+		add eax, b
+		add eax, c
+
+		mov ecx, 2
+		mov edx, 0
+		div ecx    // eax => s = (a+b+c)/2
+		mov ecx, eax
+
+
+		sub ecx, a //s-a
+		mul ecx    //eax = s*(s-a)
+
+		add ecx, a // get back to s
+
+		sub ecx, b //s-b
+		mul ecx    //eax = s * (s - a) * (s - b)
+
+		add ecx, b // get back to s
+
+		sub ecx, c //s-c
+		mul ecx    //eax = s*(s-a)*(s-b)*(s-c)
+
+		mov result, eax 
 	}
+
+	return sqrt(result);
 }
 
 int main()
@@ -111,7 +145,7 @@ int main()
 	cout << perimeter_of_equilateral_triangle(7) << endl;
 	cout << perimeter_of_rectangular_triangle(6, 10) << endl;
 	cout << area_of_cube(10) << endl;
-	cout << perimeter_of_triangle_heron_formula(10, 8, 7) << endl;
+	cout << area_of_triangle_heron_formula(10, 7, 7) << endl;
 
 
 }
